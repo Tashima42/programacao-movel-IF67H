503d8b61ed944848fc5e2583ec6cdb87be9592d7
Pedro Tashima <pedrotashima42@gmail.com>
2022-09-04 10:14:16 -0300

    vaccine view

--- pages/Vaccines.js
+++ pages/Vaccines.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
 import React, { useState } from 'react';
-import iconVaccine from "../assets/icon-vaccine.png"
 import Vaccine from '../components/Vaccine';
 import Header from "../components/Header"
 
@@ -38,14 +37,6 @@ export default function Vaccines({ navigation }) {
       img: "https://firebasestorage.googleapis.com/v0/b/projeto-mobile-7af32.appspot.com/o/public%2Fface2.png?alt=media",
       nextDose: "Próxima dose em 11/10/2022"
     },
-    {
-      id: 5,
-      title: "Poliomelite",
-      dose: "1a. dose",
-      date: "11/06/2022",
-      img: "https://firebasestorage.googleapis.com/v0/b/projeto-mobile-7af32.appspot.com/o/public%2Fface2.png?alt=media",
-      nextDose: "Próxima dose em 11/10/2022"
-    },
   ])
   function createVaccines(vaccines) {
     const r = Math.floor(vaccines.length / 2)
@@ -71,17 +62,19 @@ export default function Vaccines({ navigation }) {
   }
   return (
     <View style={styles.container}>
-      <Header navigation={navigation}/>
-      <View className="vaccines">
-        <FlatList
-          data={createVaccines(vaccines)}
-          renderItem={renderVaccine}
-          keyExtractor={item => item.id}
-          numColumns={2}
-        />
-      </View>
-      <View className="form-buttons">
-        <Button title="Nova vacina" color="green" />
+      <Header navigation={navigation} />
+      <View style={styles.body}>
+        <View className="vaccines">
+          <FlatList
+            data={createVaccines(vaccines)}
+            renderItem={renderVaccine}
+            keyExtractor={item => item.id}
+            numColumns={2}
+          />
+        </View>
+        <View className="form-buttons" style={styles.buttonContainer}>
+          <Button title="Nova vacina" color="green" />
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -96,20 +89,12 @@ const styles = StyleSheet.create({
     alignItens: "center",
     backgroundColor: "#ADD4D0",
   },
-  image: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  title: {
-    flex: 1,
-    flexDirection: "row",
-    text: {
-      fontSize: "30px",
-      textDecorationLine: 'underline'
-    },
-    height: "30px"
-  },
   centerText: {
     fontSize: "30px"
+  },
+  buttonContainer: { },
+  body: {
+    flex: 1,
+    alignItens: 'flex-end',
   }
 });
